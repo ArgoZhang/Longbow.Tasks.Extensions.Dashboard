669a637fe0ae67806ee6971257abe12f267c08e2
Argo-Tianyi <argo@163.com>
2021-10-17 21:44:09 +0800

    init: 初始化仓库

---
+++ src/Longbow.Tasks.Extensions.Dashboard.Shared/Pages/TableDemo.razor.cs
@@ -0,0 +1,34 @@
+﻿using BootstrapBlazor.Components;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Longbow.Tasks.Extensions.Dashboard.Shared.Pages
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class TableDemo : ComponentBase
+    {
+        [Inject]
+        private IStringLocalizer<Foo> Localizer { get; set; }
+
+        private IEnumerable<SelectedItem> Hobbys { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+
+            Hobbys = Foo.GenerateHobbys(Localizer);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private IEnumerable<int> PageItemsSource => new int[] { 4, 10, 20 };
+    }
+}
